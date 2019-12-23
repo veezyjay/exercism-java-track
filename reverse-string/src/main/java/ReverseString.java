88810b197369cc5acf4e77aadb02d5a5c9fef567
Victor Bassey <viktorbasi545@gmail.com>
2019-12-23 20:37:40 +0100

    Add reverse string solution

---
+++ reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,8 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        StringBuilder output = new StringBuilder();
+        return output.append(inputString).reverse().toString();
+    }
+  
+}
