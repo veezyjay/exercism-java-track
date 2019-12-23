daccd5b275309b31cb36236c0cafecb0349f9231
Victor Bassey <viktorbasi545@gmail.com>
2019-12-23 20:38:52 +0100

    Add leap solution

---
+++ leap/src/main/java/Leap.java
@@ -0,0 +1,14 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && year % 400 != 0) {
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
